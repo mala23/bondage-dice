b10608a22fac6ed3ee7aa5bbb2ec21ae32257e35
mala23 <mail@mala23.me>
2019-09-20 20:31:28 +0800

    implement mqtt con

--- dice/dice.ino
+++ dice/dice.ino
@@ -1,81 +1,60 @@
-#include <PubSubClient.h>
 #include <WiFi.h>
+#include <MQTT.h>
 
-const char* ssid = "The Grand Blossom 27A";
-const char* password =  "TGB_guest27a";
+const char ssid[] = "kali";
+const char pass[] = "WoollyBugger837";
 
-const int statusLED = LED_BUILTIN;
+WiFiClient net;
+MQTTClient client;
 
-const int switchTilt01 = 26;
-const int switchTilt02 = 25;
-const int switchTilt03 = 34;
-const int switchTilt04 = 39;
-const int switchTilt05 = 36;
-const int switchTilt06 = 4;
+unsigned long lastMillis = 0;
 
-int val1 = 0;
-int val2 = 0;
-int val3 = 0;
-int val4 = 0;
-int val5 = 0;
-int val6 = 0;
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino", "try", "try")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/liberatehongkong");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+}
 
 void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
 
-  // Initialize serial
-  Serial.begin(9600);
- 
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-  }
- 
-  Serial.println("Connected to the WiFi network");
-  
-  pinMode (statusLED, OUTPUT);
-  
-  pinMode (switchTilt01, INPUT);
-  pinMode (switchTilt02, INPUT);
-  pinMode (switchTilt03, INPUT);
-  pinMode (switchTilt04, INPUT);
-  pinMode (switchTilt05, INPUT);
-  pinMode (switchTilt06, INPUT);
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+  // You need to set the IP address directly.
+  client.begin("172.20.10.12", net);
+  client.onMessage(messageReceived);
 
-  Serial.begin(9600);
-
+  connect();
 }
 
 void loop() {
-  val1 = digitalRead(switchTilt01);
-  val2 = digitalRead(switchTilt02);
-  val3 = digitalRead(switchTilt03);
-  val4 = digitalRead(switchTilt04);
-  val5 = digitalRead(switchTilt05);
-  val6 = digitalRead(switchTilt06);
-  
-  if (val1 == LOW) {
-    Serial.println("Side 1");
+  client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
   }
 
-  if (val2 == LOW) {
-    Serial.println("Side 2");
-  }
-  
-  if (val3 == LOW) {
-    Serial.println("Side 3");
-  }
-  
-  if (val4 == LOW) {
-    Serial.println("Side 4");
-  }
-
-  if (val5 == LOW) {
-    Serial.println("Side 5");
-  }
-
-  if (val6 == LOW) {
-    Serial.println("Side 6");
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("/liberatehongkong", "liberatehonkong");
   }
 }
