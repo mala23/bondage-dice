ad44245a022227ba23fddb6fa69b110878cd580b
mala23 <mail@mala23.me>
2019-09-23 20:32:08 +0800

    get mqtt running

--- dice/dice.ino
+++ dice/dice.ino
@@ -4,6 +4,23 @@
 const char ssid[] = "kali";
 const char pass[] = "WoollyBugger837";
 
+//Initiate tiltswitches pins
+const int statusLED = LED_BUILTIN;
+const int switchTilt01 = 26;
+const int switchTilt02 = 25;
+const int switchTilt03 = 34;
+const int switchTilt04 = 39;
+const int switchTilt05 = 36;
+const int switchTilt06 = 4;
+
+//Initiate tiltswitches values
+int val1 = 0;
+int val2 = 0;
+int val3 = 0;
+int val4 = 0;
+int val5 = 0;
+int val6 = 0;
+
 WiFiClient net;
 MQTTClient client;
 
@@ -42,6 +59,18 @@ void setup() {
   client.onMessage(messageReceived);
 
   connect();
+
+  //Set led pins
+  pinMode (statusLED, OUTPUT);
+
+  //Set tiltswitches pins
+  pinMode (switchTilt01, INPUT);
+  pinMode (switchTilt02, INPUT);
+  pinMode (switchTilt03, INPUT);
+  pinMode (switchTilt04, INPUT);
+  pinMode (switchTilt05, INPUT);
+  pinMode (switchTilt06, INPUT);
+
 }
 
 void loop() {
@@ -56,5 +85,43 @@ void loop() {
   if (millis() - lastMillis > 1000) {
     lastMillis = millis();
     client.publish("/liberatehongkong", "liberatehonkong");
+  }
+  
+  //Read tiltswitches
+  val1 = digitalRead(switchTilt01);
+  val2 = digitalRead(switchTilt02);
+  val3 = digitalRead(switchTilt03);
+  val4 = digitalRead(switchTilt04);
+  val5 = digitalRead(switchTilt05);
+  val6 = digitalRead(switchTilt06);
+
+  if (val1 == LOW) {
+    Serial.println("Side 1");
+    client.publish("/dice", "1");
+  }
+
+  if (val2 == LOW) {
+    Serial.println("Side 2");
+    client.publish("/dice", "2");
+  }
+    
+  if (val3 == LOW) {
+    Serial.println("Side 3");
+    client.publish("/dice", "3");
+  }
+    
+  if (val4 == LOW) {
+    Serial.println("Side 4");
+    client.publish("/dice", "4");
+  }
+
+  if (val5 == LOW) {
+    Serial.println("Side 5");
+    client.publish("/dice", "5");
+  }
+
+  if (val6 == LOW) {
+    Serial.println("Side 6");
+    client.publish("/dice", "6");
   }
 }
